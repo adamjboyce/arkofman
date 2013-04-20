11db115085268b77143b106bfeb0234de8d63e9b
adamjboyce <adamjboyce@gmail.com>
2013-04-20 16:40:13 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arkofman::Application.config.secret_token = '0fd911c2041c7c8cad7844f9edde04bbf216e9822cbab50711919615ab60ed3e83a5b95dbba45e0d9721f86c106217dd5df4a49896479ca7225314e14647749e'
